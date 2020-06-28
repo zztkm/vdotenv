8b63ee470fd44bc3753fafcf32b5d7c0733a64ac
zztkm <takumi.basket1682@gmail.com>
2020-06-28 14:50:31 +0900

    fix: cut function

--- vdotenv.v
+++ vdotenv.v
@@ -2,6 +2,7 @@ module vdotenv
 
 import os
 
+//.envファイルから環境変数を読み込んで設定する.
 pub fn load() {
         path := '.env'
         contents := os.read_file(path.trim_space()) or {
@@ -10,15 +11,10 @@ pub fn load() {
         }
         lines := contents.split_into_lines()
         pairs := parse_lines(lines)
-        for pair in pairs.keys() {
-                mut key := pair
-                mut value := pairs[key]
-                // 現状は環境変数を上書きしないようにしている
-                os.setenv(key, value, false)
-        }
+        parse_set_env(pairs)
 }
 
-// 環境変数を key&valueの組で保存
+// 環境変数を key&valueの組で保存.
 fn parse_lines(lines []string) map[string]string {
         mut dict := map[string]string{}
         for line in lines {
@@ -27,4 +23,14 @@ fn parse_lines(lines []string) map[string]string {
                 dict[key] = value
         }
         return dict
+}
+
+// 渡されたmapを解析して環境変数を設定する.
+fn parse_set_env(pairs map[string]string) {
+        for pair in pairs.keys() {
+                mut key := pair
+                mut value := pairs[key]
+                // 現状は環境変数を上書きしないようにしている
+                os.setenv(key, value, false)
+        }
 }
