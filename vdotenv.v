97059a63191aa0c4bd8f2405d368dc5f5fa243d6
zztkm <takumi.basket1682@gmail.com>
2020-07-22 00:34:53 +0900

    add: over_load()

--- vdotenv.v
+++ vdotenv.v
@@ -4,33 +4,38 @@ import os
 
 //.envファイルから環境変数を読み込んで設定する.
 pub fn load() {
-    path := '.env'
-    contents := os.read_file(path.trim_space()) or {
-        println('Failed to open $path')
+    filename := '.env'
+    load_file(filename, false)
+}
+
+//環境変数を上書きする.
+pub fn over_load() {
+    filename := '.env'
+    load_file(filename, true)
+}
+
+fn load_file(filename string, over_load bool) {
+    contents := os.read_file(filename.trim_space()) or {
+        println('Failed to open $filename')
         return
     }
     lines := contents.split_into_lines()
-    pairs := parse_lines(lines)
-    parse_set_env(pairs)
+    env_map := parse_lines(lines)
+    for env in env_map.keys() {
+        key := env
+        value := env_map[key]
+        // 現状は環境変数を上書きしないようにしている
+        os.setenv(key, value, over_load)
+    }
 }
 
-// 環境変数を key&valueの組で保存.
+// env file から読み込んだ値を keys and values で返却する.
 fn parse_lines(lines []string) map[string]string {
-    mut dict := map[string]string{}
+    mut env_map := map[string]string{}
     for line in lines {
         key := line.split('=')[0]
         value := line.split('=')[1]
-        dict[key] = value
+        env_map[key] = value
     }
-    return dict
-}
-
-// 渡されたmapを解析して環境変数を設定する.
-fn parse_set_env(pairs map[string]string) {
-    for pair in pairs.keys() {
-        key := pair
-        value := pairs[key]
-        // 現状は環境変数を上書きしないようにしている
-        os.setenv(key, value, false)
-    }
+    return env_map 
 }
