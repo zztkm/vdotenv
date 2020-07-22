f8c84315f135deea40a1df77a8d57b5de682b508
Takumi Tsuruta <ttsuruta@landscape.co.jp>
2020-07-22 12:54:58 +0900

    add: test.

---
+++ vdotenv_test.v
@@ -0,0 +1,20 @@
+module vdotenv
+
+import os
+
+
+fn test_load() {
+    // loads env vars from a .env file.
+    load()
+
+    env_var := os.getenv('TEST2')
+	assert env_var == 'LOADENV'
+}
+
+fn test_over_load() {
+	// over loads env vars from .env file.
+	over_load()
+
+    env_var := os.getenv('TEST')
+	assert env_var == 'OVERLOADENV'
+}
