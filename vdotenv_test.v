11a8c1d0bc80a0246d8b953653b90915ff9bbdee
zztkm <takumi.basket1682@gmail.com>
2020-10-24 15:15:02 +0900

    Format vdotenv_test.v

--- vdotenv_test.v
+++ vdotenv_test.v
@@ -2,19 +2,16 @@ module vdotenv
 
 import os
 
-
 fn test_load() {
-    // loads env vars from a .env file.
-    load()
-
-    env_var := os.getenv('TEST2')
+	// loads env vars from a .env file.
+	load()
+	env_var := os.getenv('TEST2')
 	assert env_var == 'LOADENV'
 }
 
 fn test_over_load() {
 	// over loads env vars from .env file.
 	over_load()
-
-    env_var := os.getenv('TEST')
+	env_var := os.getenv('TEST')
 	assert env_var == 'OVERLOADENV'
 }
